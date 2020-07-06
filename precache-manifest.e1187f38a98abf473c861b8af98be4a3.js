12bf4ee7a517adf92b9a7dca4d3be7f7c74603cf
Mohit Chachra <chachramohit@gmail.com>
2020-07-06 22:52:34 +0530

    Updates

---
+++ precache-manifest.e1187f38a98abf473c861b8af98be4a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d070b3ea9fe15da3a71ff174d4ac21a",
+    "url": "/Dice-React/index.html"
+  },
+  {
+    "revision": "de38441e755e4d66824a",
+    "url": "/Dice-React/static/css/main.29fe419b.chunk.css"
+  },
+  {
+    "revision": "8aabc6bb0cf69108aa53",
+    "url": "/Dice-React/static/js/2.450296cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dice-React/static/js/2.450296cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de38441e755e4d66824a",
+    "url": "/Dice-React/static/js/main.296686db.chunk.js"
+  },
+  {
+    "revision": "67afb4a6b19699dc416f",
+    "url": "/Dice-React/static/js/runtime-main.bc3afeb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dice-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
